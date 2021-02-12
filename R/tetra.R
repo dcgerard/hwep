07d86a8c157017e185e68effd9c40bed1a99cdc5
David Gerard <gerard.1787@gmail.com>
2021-02-12 16:22:08 -0500

    change alternative hypothesis in chisq_hwe of hwetetra()

--- R/tetra.R
+++ R/tetra.R
@@ -79,9 +79,9 @@ lltetra <- function(a, r, nvec) {
 #'  \item{\code{ll_u}}{The log-likelihood under the urestricted model.}
 #'  \item{\code{q_rm}}{The genotype frequencies under the random mating model.}
 #'  \item{\code{ll_rm}}{The log-likelihood under the random mating model.}
-#'  \item{\code{q_hw}}{The genotype frequencies under the small deviations
+#'  \item{\code{q_hwe}}{The genotype frequencies under the small deviations
 #'      from HWE model.}
-#'  \item{\code{ll_hw}}{The log-likelihood under the small deviations from HWE
+#'  \item{\code{ll_hwe}}{The log-likelihood under the small deviations from HWE
 #'      model.}
 #'  \item{\code{q_ndr}}{The genotype frequencies under the no double reduction
 #'      at HWE model.}
@@ -91,10 +91,11 @@ lltetra <- function(a, r, nvec) {
 #'      mating, with an alternative of unrestricted.}
 #'  \item{\code{df_rm}}{The degrees of freedom corresponding to \code{chisq_rm}.}
 #'  \item{\code{pval_rm}}{The p-value corresponding to \code{chisq_rm}.}
-#'  \item{\code{chisq_hw}}{The chi-square statistic against the null of only
-#'      small deviations from HWE, with an alternative of random mating.}
-#'  \item{\code{df_hw}}{The degrees of freedom corresponding to \code{chisq_hw}.}
-#'  \item{\code{pval_hw}}{The p-value corresponding to \code{chisq_hw}.}
+#'  \item{\code{chisq_hwe}}{The chi-square statistic against the null of only
+#'      small deviations from HWE, with an alternative of arbitrary
+#'      genotype frequencies.}
+#'  \item{\code{df_hwe}}{The degrees of freedom corresponding to \code{chisq_hwe}.}
+#'  \item{\code{pval_hwe}}{The p-value corresponding to \code{chisq_hwe}.}
 #'  \item{\code{chisq_ndr}}{The chi-square statistic against the null of no
 #'      double reduction at HWE, with an alternative of only small deviations
 #'      from HWE.}
@@ -146,10 +147,10 @@ hwetetra <- function(nvec, upperdr = 1/6, addval = 1 / 100) {
                        r = r,
                        control = list(fnscale = -1))
   alpha <- oout$par
-  p_hw <- gam_from_a(a = alpha, r = r)
-  names(p_hw) <- NULL
-  q_hw <- stats::convolve(p_hw, rev(p_hw), type = "open")
-  ll_hw <- oout$value
+  p_hwe <- gam_from_a(a = alpha, r = r)
+  names(p_hwe) <- NULL
+  q_hwe <- stats::convolve(p_hwe, rev(p_hwe), type = "open")
+  ll_hwe <- oout$value
 
   ## No dr ------------------------------------
   p_ndr <- c((1 - r) ^ 2, 2 * r * (1-r), r ^ 2)
@@ -159,17 +160,17 @@ hwetetra <- function(nvec, upperdr = 1/6, addval = 1 / 100) {
   ## q names
   names(q_u) <- 0:4
   names(q_rm) <- 0:4
-  names(q_hw) <- 0:4
+  names(q_hwe) <- 0:4
   names(q_ndr) <- 0:4
 
   ## Chi square tests -------------------------
   chisq_rm <- -2 * (ll_rm - ll_u)
   pval_rm <- stats::pchisq(q = chisq_rm, df = 2, lower.tail = FALSE)
 
-  chisq_hw <- -2 * (ll_hw - ll_rm)
-  pval_hw <- stats::pchisq(q = chisq_hw, df = 1, lower.tail = FALSE)
+  chisq_hwe <- -2 * (ll_hwe - ll_u)
+  pval_hwe <- stats::pchisq(q = chisq_hwe, df = 2, lower.tail = FALSE)
 
-  chisq_ndr <- -2 * (ll_ndr - ll_hw)
+  chisq_ndr <- -2 * (ll_ndr - ll_hwe)
   pval_ndr <- stats::pchisq(q = chisq_ndr, df = 1, lower.tail = FALSE)
 
   ## Return everything -----------------------
@@ -180,16 +181,16 @@ hwetetra <- function(nvec, upperdr = 1/6, addval = 1 / 100) {
     ll_u = ll_u,
     q_rm = q_rm,
     ll_rm = ll_rm,
-    q_hw = q_hw,
-    ll_hw = ll_hw,
+    q_hwe = q_hwe,
+    ll_hwe = ll_hwe,
     q_ndr = q_ndr,
     ll_ndr = ll_ndr,
     chisq_rm = chisq_rm,
     df_rm = 2,
     pval_rm = pval_rm,
-    chisq_hw = chisq_hw,
-    df_hw = 1,
-    pval_hw = pval_hw,
+    chisq_hwe = chisq_hwe,
+    df_hwe = 2,
+    pval_hwe = pval_hwe,
     chisq_ndr = chisq_ndr,
     df_ndr = 1,
     pval_ndr = pval_ndr)
